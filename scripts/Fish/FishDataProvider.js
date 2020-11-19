e1a7d6d8703847a4a6bada6a8565d33d8dd84244
christinaashworth <christina.ashworth@ymail.com>
2020-11-19 13:50:54 -0600

    sorted fish by length categories

--- scripts/Fish/FishDataProvider.js
+++ scripts/Fish/FishDataProvider.js
@@ -36,3 +36,53 @@ const fishCollection = [
 export const useFish = () => {
     return fishCollection.slice();
 }
+
+
+
+/* Separate the fish by type
+List the fish by those types 
+1) holy fish 
+2) soldiers
+3) unworthy
+
+Only need to work in FishDataProvider and FishList*/
+
+
+
+ export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+     const holyFish = []
+
+     for (const fish of fishCollection) {
+         if (fish.length % 3 === 0) {
+             holyFish.push(fish);
+        }
+     }
+    return holyFish
+}
+
+ export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+     const soldiers = []
+
+     for (const fish of fishCollection) {
+         if (fish.length % 5 === 0 && fish.length % 3 !== 0) {
+             soldiers.push(fish);
+         }
+     }
+    return soldiers
+}
+
+ export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+     const regularFish = []
+
+     for (const fish of fishCollection) {
+         if (fish.length % 3 !== 0 && fish.length % 5 !== 0) {
+             regularFish.push(fish);
+         }
+     }
+    return regularFish
+} 
+
+
